e63caae219ed100513fe21d7c9d4b9c0cb101a52
Colin VanLang <colinvanlang@gmail.com>
2016-02-21 17:48:25 -0500

    Fixed for Linux!

--- setup_sublime_symlinks.sh
+++ setup_sublime_symlinks.sh
@@ -1,38 +1,36 @@
 #!/usr/bin/env bash
 
-# Collect os
-os=$1
-if [ "$os" != "linux" ]
-  then
-    os="mac"
+# OS=$1
+if [ "$1" == "linux" ] || [ "$1" == "mac" ] ; then
+  OS="$1"
+else
+  echo "Please provide OS type (linux or mac)"
+  return
 fi
 
-if ["$os" == 'mac']
-then
-  sublime_path="$HOME/Library/Application Support/Sublime Text 3/Packages/User"
+if [ $OS == "mac" ]; then
+  SUBLIME_PATH="$HOME/Library/Application Support/Sublime Text 3/Packages/User"
 else
-  sublime_path="$HOME/test"
+  SUBLIME_PATH="$HOME/.config/sublime-text-3/Packages/User"
 fi
 
 ## Remove old files (if they're there)
 # rm "$sublime_path/alt_page_updown.py"
 # rm "$sublime_path/Default (OSX).sublime-keymap"
-rm ~/Library/Application\ Support/Sublime\ Text\ 3/Packages/User/alt_page_updown.py
-rm ~/Library/Application\ Support/Sublime\ Text\ 3/Packages/User/Default\ \(OSX\).sublime-keymap
-rm ~/Library/Application\ Support/Sublime\ Text\ 3/Packages/User/Package\ Control.sublime-settings
-rm ~/Library/Application\ Support/Sublime\ Text\ 3/Packages/User/Preferences.sublime-settings
-rm -rf ~/Library/Application\ Support/Sublime\ Text\ 3/Packages/User/Snippets
+rm "$SUBLIME_PATH/alt_page_updown.py"
+rm "$SUBLIME_PATH/Default (OSX).sublime-keymap"
+rm "$SUBLIME_PATH/Package Control.sublime-settings"
+rm "$SUBLIME_PATH/Preferences.sublime-settings"
+rm -rf "$SUBLIME_PATH/Snippets"
 
 rm ~/.editorconfig
 
 ## Create the symbolic links
-ln -s ~/dotfiles/Sublime/alt_page_updown.py ~/Library/Application\ Support/Sublime\ Text\ 3/Packages/User/alt_page_updown.py
-ln -s ~/dotfiles/Sublime/Default\ \(OSX\).sublime-keymap ~/Library/Application\ Support/Sublime\ Text\ 3/Packages/User/Default\ \(OSX\).sublime-keymap
-ln -s ~/dotfiles/Sublime/Package\ Control.sublime-settings ~/Library/Application\ Support/Sublime\ Text\ 3/Packages/User/Package\ Control.sublime-settings
-ln -s ~/dotfiles/Sublime/Preferences.sublime-settings ~/Library/Application\ Support/Sublime\ Text\ 3/Packages/User/Preferences.sublime-settings
-ln -s ~/dotfiles/Sublime/Snippets ~/Library/Application\ Support/Sublime\ Text\ 3/Packages/User/Snippets
+ln -s ~/dotfiles/Sublime/alt_page_updown.py "$SUBLIME_PATH/alt_page_updown.py"
+ln -s ~/dotfiles/Sublime/Default\ \(OSX\).sublime-keymap "$SUBLIME_PATH/Default (OSX).sublime-keymap"
+ln -s ~/dotfiles/Sublime/Default\ \(Linux\).sublime-keymap "$SUBLIME_PATH/Default (Linux).sublime-keymap"
+ln -s ~/dotfiles/Sublime/Package\ Control.sublime-settings "$SUBLIME_PATH/Package Control.sublime-settings"
+ln -s ~/dotfiles/Sublime/Preferences.sublime-settings "$SUBLIME_PATH/Preferences.sublime-settings"
+ln -s ~/dotfiles/Sublime/Snippets "$SUBLIME_PATH/Snippets"
 
 ln -s ~/dotfiles/.editorconfig ~/.editorconfig
-
-# Reload bash
-source ./.bashrc
