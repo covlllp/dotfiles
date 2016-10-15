f3c8ed92b0517455f7af484d80607602e20ad046
Colin VanLang <colinvanlang@gmail.com>
2016-10-15 16:19:36 -0400

    cleaned up setup script, no longer uses bash

--- setup_home_symlinks.sh
+++ setup_home_symlinks.sh
@@ -1,22 +1,16 @@
 #!/usr/bin/env bash
 
 # Remove old files
-rm ~/.bash_profile
-rm ~/.bashrc
+rm ~/.editorconfig
 rm ~/.gitconfig
+rm ~/.gitignore_global
 rm ~/.vimrc
 rm ~/.zshrc
-rm ~/.gitignore_global
-rm ~/.git-completion.bash
 
 # Setup symbolic links for files to ~/
-ln -s ~/dotfiles/.bash_profile ~/.bash_profile
-ln -s ~/dotfiles/.bashrc ~/.bashrc
-ln -s ~/dotfiles/.gitconfig ~/.gitconfig
-ln -s ~/dotfiles/.vimrc ~/.vimrc
-ln -s ~/dotfiles/.zshrc ~/.zshrc
-ln -s ~/dotfiles/.gitignore_global ~/.gitignore_global
-ln -s ~/dotfiles/.git-completion.bash ~/.git-completion.bash
+ln -s ~/dotfiles/editorconfig ~/.editorconfig
+ln -s ~/dotfiles/gitconfig ~/.gitconfig
+ln -s ~/dotfiles/gitignore_global ~/.gitignore_global
+ln -s ~/dotfiles/vimrc ~/.vimrc
+ln -s ~/dotfiles/zshrc ~/.zshrc
 
-# Reload bash
-source ./.bashrc
