d03eb3c50fafd713079eeca9fe15d0b49c0532f6
Colin VanLang <colinvanlang@gmail.com>
2016-12-21 19:25:17 +0000

    fix pureprompt script

--- load_pure_prompt.sh
+++ load_pure_prompt.sh
@@ -2,5 +2,8 @@
 
 sudo npm install -g pure-prompt
 
-sudo ln -s "/usr/lib/node_modules/pure-prompt/pure.zsh" /usr/local/share/zsh/site-functions/prompt_pure_setup
-sudo ln -s "/usr/lib/node_modules/pure-prompt/async.zsh" /usr/local/share/zsh/site-functions/prompt_pure_setup
+rm /usr/local/share/zsh/site-functions/prompt_pure_setup
+rm /usr/local/share/zsh/site-functions/async
+
+sudo ln -s "/usr/local/lib/node_modules/pure-prompt/pure.zsh" /usr/local/share/zsh/site-functions/prompt_pure_setup
+sudo ln -s "/usr//local/lib/node_modules/pure-prompt/async.zsh" /usr/local/share/zsh/site-functions/async
